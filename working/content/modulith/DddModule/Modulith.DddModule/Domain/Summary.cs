196f93d04383ec2ee5ada14d104c7a34b6f7876d
austoh80 <84339153+austoh80@users.noreply.github.com>
2024-07-12 18:18:37 +0300

    Initialize 28-cleanup-unused-templates

---
+++ working/content/modulith/DddModule/Modulith.DddModule/Domain/Summary.cs
@@ -0,0 +1,15 @@
+namespace Modulith.NewModule.Domain;
+
+internal enum Summary
+{
+    Freezing,
+    Bracing,
+    Chilly,
+    Cool,
+    Mild,
+    Warm,
+    Balmy,
+    Hot,
+    Sweltering,
+    Scorching
+}
